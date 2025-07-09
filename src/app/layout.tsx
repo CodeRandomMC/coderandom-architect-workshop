dd39ad89c273fb5728502bfd4654337e6671a2f6
Code Random <admin@coderandom.com>
2025-07-09 21:04:20 +0000

    add Vercel speed insights
    npm i @vercel/speed-insights
    import { SpeedIn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import { Inter } from 'next/font/google';
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({
   subsets: ['latin'],
@@ -25,6 +26,7 @@ export default function RootLayout({
       <body className="font-body antialiased">
         {children}
         <Toaster />
+        <SpeedInsights />
       </body>
     </html>
   );
